605cb13a4ee1acda90ba2df567416d70dd927f38
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-03-29 21:34:39 +0200

    feat(terminal): try to drag and move the terminal

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,49 @@
 const socket = io();
+const terminal = document.querySelector("#terminal");
 
-function sendMessage() {
-  socket.emit("addNewMessage", "mis huevos");
-}
+let mouseMovement;
 
-sendMessage();
+terminal.addEventListener(
+  "mousedown",
+  (e) => {
+    let initialPosition = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    terminal.addEventListener(
+      "mousemove",
+      (e) => {
+        handleMouseMove(e, initialPosition);
+      },
+      false
+    );
+  },
+  false
+);
+
+terminal.addEventListener("mouseup", () => {
+  terminal.removeEventListener(
+    "mousemove",
+    (e) => {
+      handleMouseMove(e, initialPosition);
+    },
+    false
+  );
+});
+
+const handleMouseMove = (e, initialPosition) => {
+  let { offsetX, offsetY, clientX, clientY } = e;
+  let { x, y } = terminal.getBoundingClientRect();
+  console.log("*********");
+  console.log(initialPosition);
+  // console.log("nueva posición", clientX - offsetX, clientY - offsetY);
+  // terminal.style.left = `${x + offsetX}px`;
+  // terminal.style.top = `${y + offsetY}px`;
+};
+
+// function sendMessage() {
+//   socket.emit("addNewMessage", "mis huevos");
+// }
+
+// sendMessage();

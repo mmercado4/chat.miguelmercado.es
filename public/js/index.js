c7fe1c645a36122e46a5cda63e4a6292be5a3698
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-03-31 08:59:06 +0200

    feat(terminal): develop terminal drag function

--- public/js/index.js
+++ public/js/index.js
@@ -1,45 +1,45 @@
 const socket = io();
 const terminal = document.querySelector("#terminal");
 
-let mouseMovement;
+let mouseDown = false;
+let mouseX = 0;
+let mouseY = 0;
+//Ponerlo en un archivo separado y poner solo en la barra superior de la terminal
 
 terminal.addEventListener(
   "mousedown",
   (e) => {
-    let initialPosition = {
-      x: e.clientX,
-      y: e.clientY,
-    };
-
-    terminal.addEventListener(
-      "mousemove",
-      (e) => {
-        handleMouseMove(e, initialPosition);
-      },
-      false
-    );
+    e.preventDefault();
+    mouseDown = true;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
   },
   false
 );
 
-terminal.addEventListener("mouseup", () => {
-  terminal.removeEventListener(
-    "mousemove",
-    (e) => {
-      handleMouseMove(e, initialPosition);
-    },
-    false
-  );
+terminal.addEventListener(
+  "mouseup",
+  () => {
+    mouseDown = false;
+    console.log(terminal.getBoundingClientRect());
+    let { x, y } = terminal.getBoundingClientRect();
+    terminal.style.left = `${x}px`;
+    terminal.style.top = `${y}px`;
+    terminal.style.transform = `translate(0px, 0px)`;
+  },
+  false
+);
+
+terminal.addEventListener("mousemove", (e) => {
+  e.preventDefault();
+  if (!mouseDown) return;
+  let deltaX = e.clientX - mouseX;
+  let deltaY = e.clientY - mouseY;
+  handleMouseMove(deltaX, deltaY);
 });
 
-const handleMouseMove = (e, initialPosition) => {
-  let { offsetX, offsetY, clientX, clientY } = e;
-  let { x, y } = terminal.getBoundingClientRect();
-  console.log("*********");
-  console.log(initialPosition);
-  // console.log("nueva posición", clientX - offsetX, clientY - offsetY);
-  // terminal.style.left = `${x + offsetX}px`;
-  // terminal.style.top = `${y + offsetY}px`;
+const handleMouseMove = (deltaX, deltaY) => {
+  terminal.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
 };
 
 // function sendMessage() {
